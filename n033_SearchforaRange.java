10f2441a7a8447afe57b5397d75cc4db4e0e1b07
Shize Xu <xszsmail@gmail.com>
2013-08-26 08:50:45 -0700

    To strictly optimized logN complexity.

--- n033_SearchforaRange.java
+++ n033_SearchforaRange.java
@@ -1,34 +1,42 @@
 package LeetCode;
 
 public class n033_SearchforaRange {
-	int sub_search(int[] A, int l, int r, int t, Boolean isLeft) {
-		if (l > r) {
-			if (isLeft)
-				return (l < A.length && A[l] == t) ? l : -1;
-			else
-				return (r >= 0 && A[r] == t) ? r : -1;
-		}
-		int m = (l + r) / 2;
-		if (A[m] < t)
-			return sub_search(A, m + 1, r, t, isLeft);
-		if (A[m] > t)
-			return sub_search(A, l, m - 1, t, isLeft);
-		// else if A[m]==t
-		if (isLeft)
-			return sub_search(A, l, m - 1, t, isLeft);
-		return sub_search(A, m + 1, r, t, isLeft);
-	}
-
 	public int[] searchRange(int[] A, int target) {
 		// Start typing your Java solution below
 		// DO NOT write main() function
-		int n = A.length;
-		if (n < 1) {
+		int l = 0, r = A.length - 1;
+		int mid = 0;
+		while (l <= r) { // find an arbitrary target in array
+			mid = l + (r - l) / 2;
+			if (A[mid] > target)
+				r = mid - 1;
+			else if (A[mid] < target)
+				l = mid + 1;
+			else
+				break;
+		}
+		if (l > r) {
 			int[] res = { -1, -1 };
 			return res;
 		}
-		int[] res = { sub_search(A, 0, n - 1, target, true),
-				sub_search(A, 0, n - 1, target, false) };
+		// new range has been limited to [l,r]
+		int newL = mid;
+		while (newL <= r) {
+			int m = newL + (r - newL) / 2;
+			if (A[m] <= target)
+				newL = m + 1;
+			else
+				r = m - 1;
+		}
+		int newR = mid;
+		while (newR >= l) {
+			int m = l + (newR - l) / 2;
+			if (A[m] >= target)
+				newR = m - 1;
+			else
+				l = m + 1;
+		}
+		int[] res = { l, r };
 		return res;
 	}
 }
