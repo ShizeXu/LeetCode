383eac738e451cf9825b42ccf89b6764ec63f289
Shize Xu <xszsmail@gmail.com>
2013-08-26 05:47:55 -0700

    Add some previous solved problems.

--- n069_SqrtX.java
+++ n069_SqrtX.java
@@ -1,13 +1,15 @@
 package LeetCode;
+
 public class n069_SqrtX {
-    public int sqrt(int x) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        double tmp = x/2.0;
-        while(Math.abs(tmp*tmp-x)>0.01) {
-            tmp += x/tmp;
-            tmp /= 2;
-        }
-        return (int)tmp;
-    }
+	public int sqrt(int x) {
+		// Start typing your Java solution below
+		// DO NOT write main() function
+		double a = x / 2.0;
+		double tmp = x / a; // to avoid overflow
+		while (Math.abs(a - tmp) > 0.01) {
+			a += (tmp + a) / 2.0;
+			tmp = x / a;
+		}
+		return (int) a;
+	}
 }
