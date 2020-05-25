1d6e43b70100964aba947a232b037ea33432068b
Shize <xszsmail@gmail.com>
2020-05-25 15:19:19 -0700

    898

--- 2020/Run.cpp
+++ 2020/Run.cpp
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 #include <898. Bitwise ORs of Subarrays.cpp>
 
 int main()
 {
     P_898 test;
-=======
-#include <201. Bitwise AND of Numbers Range.cpp>
-
-int main()
-{
-    P_201 test;
->>>>>>> 3659cdb77088650019881affdcbc3c684ccfd8b0
     test.RunTest();
 }
